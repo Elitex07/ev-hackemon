be5940d769e1077868c558efcf33c1018bdc1c81
priyanshubarai <priyanshubarai45@gmail.com>
2025-04-26 00:37:57 +0530

    second

--- components/SafetyMonitor.jsx
+++ components/SafetyMonitor.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import "./SafetyMonitor.css"; // Optional for styles
+import "./SafetyMonitor.css";
 
 const geoFence = {
   north: 50,
